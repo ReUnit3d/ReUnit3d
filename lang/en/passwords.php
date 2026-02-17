12ec5f3c893808c7d9b19dd8f7347231e75365ed
Oha-you <82098328+Oha-you@users.noreply.github.com>
2026-02-17 05:28:27 +0300

    update: Password reset message, change Thanks to Success

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -27,7 +27,7 @@ return [
 
     'password' => 'Passwords must be at least six characters and match the confirmation.',
     'reset'    => 'Your password has been reset!',
-    'sent'     => 'Thanks! If the email address matches an account, a password reset link will be sent.',
+    'sent'     => 'Success: If the email address matches an account, a password reset link will be sent.',
     'token'    => 'This password reset token is invalid.',
     'user'     => 'We can\'t find a user with that e-mail address.',
 ];
