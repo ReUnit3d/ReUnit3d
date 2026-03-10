44aaaed635a7ef61a87ac373c0af7a803701512d
Roardom <roardom@protonmail.com>
2026-03-10 23:30:25 +0000

    update: optimize file-tree display
    Reduces html quanity on discs with 10s of thousands of files (35MB -> 27MB). Should be optimized more properly in the future, but this is low-lying fruit.

--- resources/views/torrent/partials/file-tree-node.blade.php
+++ resources/views/torrent/partials/file-tree-node.blade.php
@@ -7,40 +7,18 @@
             'list-style-type: none' => $node['type'] === 'file',
         ])
     >
-        <span
-            style="
-                display: grid;
-                grid-template-areas: 'icon2 folder count . size';
-                grid-template-columns: 24px auto auto 1fr auto;
-                gap: 4px;
-            "
-        >
+        <span class="file-tree__wrapper">
             @if ($node['type'] === 'file')
-                <i class="{{ config('other.font-awesome') }} fa-file" style="grid-area: icon2"></i>
-                <span style="word-break: break-all">
-                    {{ $key }}
-                </span>
-                <span
-                    style="grid-area: size; white-space: nowrap; text-align: right"
-                    title="{{ $node['size'] }}&nbsp;B"
-                >
+                <i class="{{ config('other.font-awesome') }} fa-file file-tree__icon"></i>
+                <span class="file-tree__name">{{ $key }}</span>
+                <span class="file-tree__size" title="{{ $node['size'] }}&nbsp;B">
                     {{ App\Helpers\StringHelper::formatBytes($node['size'], 2) }}
                 </span>
             @else
-                <i
-                    class="{{ config('other.font-awesome') }} fa-folder"
-                    style="grid-area: icon2"
-                ></i>
-                <span>
-                    {{ $key }}
-                </span>
-
-                <span style="grid-area: count">({{ $node['count'] }})</span>
-                <span
-                    class="text-info"
-                    style="grid-area: size; white-space: nowrap; text-align: right"
-                    title="{{ $node['size'] }}&nbsp;B"
-                >
+                <i class="{{ config('other.font-awesome') }} fa-folder file-tree__icon"></i>
+                <span class="file-tree__name">{{ $key }}</span>
+                <span class="file-tree__count">({{ $node['count'] }})</span>
+                <span class="text-info file-tree__size" title="{{ $node['size'] }}&nbsp;B">
                     {{ App\Helpers\StringHelper::formatBytes($node['size'], 2) }}
                 </span>
             @endif
