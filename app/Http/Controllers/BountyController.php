HDInnovations/UNIT3D

refactor: save request last bounty timestamp separately
created_at is the wrong field to save this value in because the initial created_at date is lost. Using updated_at also doesn't work since updating the request fields will bump the request even if more bounty isn't added. So, we use a new field for this info. We use a field on the requests table and not the request_bounty table so that we can use an index to sort.

app/Http/Controllers/BountyController.php
@@ -50,7 +50,7 @@ class BountyController extends Controller
 
         $torrentRequest->increment('bounty', $request->integer('seedbonus'));
 
-        $torrentRequest->created_at = now();
+        $torrentRequest->last_bountied_at = now();
         $torrentRequest->save();
 
         if ($request->boolean('anon') == 0) {

app/Http/Livewire/TorrentRequestSearch.php
@@ -113,7 +113,7 @@ class TorrentRequestSearch extends Component
     public int $perPage = 25;
 
     #[Url(history: true)]
-    public string $sortField = 'created_at';
+    public string $sortField = 'last_bountied_at';
 
     #[Url(history: true)]
     public string $sortDirection = 'desc';

resources/views/livewire/torrent-request-search.blade.php
@@ -336,9 +336,9 @@
                             <i class="{{ config('other.font-awesome') }} fa-coins"></i>
                             @include('livewire.includes._sort-icon', ['field' => 'bounty'])
                         </th>
-                        <th wire:click="sortBy('created_at')" role="columnheader button">
+                        <th wire:click="sortBy('last_bountied_at')" role="columnheader button">
                             {{ __('common.created_at') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'created_at'])
+                            @include('livewire.includes._sort-icon', ['field' => 'last_bountied_at'])
                         </th>
                         <th>{{ __('common.status') }}</th>
                     </tr>
@@ -367,10 +367,10 @@
                             <td>{{ number_format($torrentRequest->bounty) }}</td>
                             <td>
                                 <time
-                                    datetime="{{ $torrentRequest->created_at }}"
-                                    title="{{ $torrentRequest->created_at }}"
+                                    datetime="{{ $torrentRequest->last_bountied_at }}"
+                                    title="{{ $torrentRequest->last_bountied_at }}"
                                 >
-                                    {{ $torrentRequest->created_at->diffForHumans() }}
+                                    {{ $torrentRequest->last_bountied_at->diffForHumans() }}
                                 </time>
                             </td>
                             <td>

ReUnit3d/ReUnit3d

app/Http/Controllers/BountyController.php
@@ -50,7 +50,7 @@ class BountyController extends Controller
 
         $torrentRequest->increment('bounty', $request->integer('seedbonus'));
 
-        $torrentRequest->created_at = now();
+        $torrentRequest->bumped_at = now();
         $torrentRequest->save();
 
         if ($request->boolean('anon') == 0) {

app/Http/Livewire/TorrentRequestSearch.php
@@ -113,7 +113,7 @@ class TorrentRequestSearch extends Component
     public int $perPage = 25;
 
     #[Url(history: true)]
-    public string $sortField = 'created_at';
+    public string $sortField = 'bumped_at';
 
     #[Url(history: true)]
     public string $sortDirection = 'desc';

resources/views/livewire/torrent-request-search.blade.php
@@ -336,9 +336,9 @@
                             <i class="{{ config('other.font-awesome') }} fa-coins"></i>
                             @include('livewire.includes._sort-icon', ['field' => 'bounty'])
                         </th>
-                        <th wire:click="sortBy('created_at')" role="columnheader button">
+                        <th wire:click="sortBy('bumped_at')" role="columnheader button">
                             {{ __('common.created_at') }}
-                            @include('livewire.includes._sort-icon', ['field' => 'created_at'])
+                            @include('livewire.includes._sort-icon', ['field' => 'bumped_at'])
                         </th>
                         <th>{{ __('common.status') }}</th>
                     </tr>
@@ -367,10 +367,10 @@
                             <td>{{ number_format($torrentRequest->bounty) }}</td>
                             <td>
                                 <time
-                                    datetime="{{ $torrentRequest->created_at }}"
-                                    title="{{ $torrentRequest->created_at }}"
+                                    datetime="{{ $torrentRequest->bumped_at }}"
+                                    title="{{ $torrentRequest->bumped_at }}"
                                 >
-                                    {{ $torrentRequest->created_at->diffForHumans() }}
+                                    {{ $torrentRequest->bumped_at->diffForHumans() }}
                                 </time>
                             </td>
                             <td>